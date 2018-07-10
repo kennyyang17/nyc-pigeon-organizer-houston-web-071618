4f7a1133407a72d669026d8467cfa8698e1fc4d4
Kenny Yang <kennyyang19@gmail.com>
2018-07-10 13:33:48 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |category, info|
+    info.each do |attributes, names|
+      names.each do |name|
+        new_data[name] ||= {}
+        new_data[name][category] ||= []
+        new_data[name][category]<< attributes.to_s
+      end
+    end
+  end
+  new_data
 end
